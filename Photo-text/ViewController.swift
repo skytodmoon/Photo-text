833732fdb010b2d0b0f83723da1fd234cf25a0ff
LDSmallCat <1026190993@qq.com>
2017-10-10 20:17:26 +0800

    图文混排简单实现

--- Photo-text/ViewController.swift
+++ Photo-text/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let attrStr1 = NSAttributedString(string: "LDSmallCat", attributes: [NSAttributedStringKey.foregroundColor : UIColor.red])
+        let attrStr2 = NSAttributedString(string: "LDSmallCat", attributes: [NSAttributedStringKey.foregroundColor : UIColor.blue])
+        
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage.init(named: "d_aini")
+        let font = photoTextLabel.font
+        
+        attachment.bounds = CGRect(x: 0, y: -4, width: font!.lineHeight, height: font!.lineHeight)
+        let attImageStr = NSAttributedString(attachment: attachment)
+        
+        
+        
+        
+        let attMstr = NSMutableAttributedString()
+        attMstr.append(attrStr1)
+        attMstr.append(attImageStr)
+        attMstr.append(attrStr2)
+        photoTextLabel.attributedText = attMstr
+        
     }
 
     override func didReceiveMemoryWarning() {
